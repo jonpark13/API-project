4f200c34608cd80af15ee05ad1df0b35012dffa9
Jon Park <jonpark13@gmail.com>
2022-09-19 16:04:40 -0400

    songs, add, search start

--- frontend/src/store/songs.js
+++ frontend/src/store/songs.js
@@ -1,12 +1,28 @@
-import { csrfFetch } from './csrf';
+import { csrfFetch } from './csrf'
 
-const GET_SONGS = 'songs/getSongs';
+const GET_SONGS = 'songs/getSongs'
+const ADD_SONG = 'songs/addSong'
+const FIND_SONG = 'songs/findSong'
 
 const getSongs = (songs) => {
-  return {
-    type: GET_SONGS,
-    songs
-  }
+    return {
+        type: GET_SONGS,
+        songs
+    }
+}
+
+const findSong = (song) => {
+    return {
+        type: FIND_SONG,
+        song
+    }
+}
+
+const addSong = (song) => {
+    return {
+        type: ADD_SONG,
+        song
+    }
 }
 
 export const songsGrab = () => async (dispatch) => {
@@ -17,18 +33,63 @@ export const songsGrab = () => async (dispatch) => {
     return data;
 };
 
+export const lookupSong = (song) => async (dispatch) => {
+    const {
+        userId,
+        albumId,
+        title,
+        description,
+        url,
+        previewImage } = song
+    const response = await csrfFetch('/api/songs', {
+        method: 'POST',
+        body: JSON.stringify({
+            credential,
+            password,
+        })
+    })
+    const data = await response.json()
+    // console.log(data)
+    dispatch(getSongs(data))
+    return data;
+};
+
+export const createSong = (song) => async (dispatch) => {
+    const {
+        userId,
+        albumId,
+        title,
+        description,
+        url,
+        previewImage } = song
+    const response = await csrfFetch('/api/songs', {
+        method: 'POST',
+        body: JSON.stringify({
+            credential,
+            password,
+        })
+    })
+    const data = await response.json()
+    // console.log(data)
+    dispatch(getSongs(data))
+    return data;
+};
+
 const initialState = {}
 
 const songsReducer = (state = initialState, action) => {
-  let newState;
-  switch (action.type) {
-    case GET_SONGS:
-      newState = Object.assign({}, state)
-      newState = action.songs
-      return newState
-    default:
-      return state
-  }
+    let newState;
+    switch (action.type) {
+        case GET_SONGS:
+            newState = Object.assign({}, state)
+            newState = action.songs
+            return newState
+        case ADD_SONG:
+            newState = Object.assign({}, state)
+            newState = action.songs
+        default:
+            return state
+    }
 };
 
 export default songsReducer;
